9bdbc18b38b11d15f4fcf8d48efa877f44f6c880
jmckeev <43043182+jmckeev@users.noreply.github.com>
2020-12-06 16:04:12 -0500

    Create StudentHandler.java

---
+++ StudentHandler.java
@@ -0,0 +1,19 @@
+public class StudentHandler {
+    private StudentDriver studentDriver;
+    private Initializer initializer;
+
+    public StudentHandler(StudentDriver driver, Initializer initializer) {
+        this.studentDriver = driver;
+        this.initializer = initializer;
+    }
+
+    public void handle(StudentDriver.STUDENT_STATE state) {
+        if (state == StudentDriver.STUDENT_STATE.userClasses) {
+            UserClasses userClasses = new UserClasses(this);
+        } else if (state == StudentDriver.STUDENT_STATE.friendSchedule) {
+            //TODO: write FriendSchedule class (GUI) and instantiate an object here
+        } else if (state == StudentDriver.STUDENT_STATE.addRemove) {
+            //TODO: write FriendSchedule class (GUI) and instantiate an object here
+        }
+    }
+}
