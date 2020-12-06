2dbc97069246e449a1073c3c6640470d24e3d39d
jmckeev <43043182+jmckeev@users.noreply.github.com>
2020-12-06 16:04:26 -0500

    Create UserClasses.java

---
+++ UserClasses.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class UserClasses extends JFrame {
+    private StudentHandler studentHandler;
+
+    public UserClasses(StudentHandler studentHandler) {
+        this.studentHandler = studentHandler;
+    }
+}
